ef3472a62ecf7f52d6dc9b6c3981d4e2fe07b92f
gabriel2m <gabriel2m.contact@gmail.com>
2022-07-19 13:00:24 -0300

    Remove crud-view and search plugins

--- src/Application.php
+++ src/Application.php
@@ -65,9 +65,6 @@ class Application extends BaseApplication
 
         // Load more plugins here
         $this->addPlugin('Crud');
-        $this->addPlugin('CrudView');
-        $this->addPlugin('BootstrapUI');
-        $this->addPlugin('Search');
         $this->addPlugin('Migrations');
     }
 
